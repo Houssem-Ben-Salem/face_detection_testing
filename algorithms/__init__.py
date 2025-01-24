28c4a36c7b046ae8ec8b21615f74e104488894e2
Houssem-Ben-Salem <houssem.bensalem@supcom.tn>
2025-01-24 13:56:11 -0500

    Remove RetinaFace algorithm

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -5,7 +5,6 @@ from .traditional.viola_jones import ViolaJonesFaceDetector
 from .traditional.hog_svm import HOGSVMFaceDetector
 from .ml_based.mtcnn import MTCNNFaceDetector
 from .ml_based.mediapipe_face import MediaPipeFaceDetector
-from .ml_based.retinaface import RetinaFaceDetector
 from .object_detection.yolo import YOLOFaceDetector
 
 def load_algorithms() -> Dict:
@@ -14,7 +13,7 @@ def load_algorithms() -> Dict:
     config_path = Path("config/algorithms.yaml")
     if not config_path.exists():
         raise FileNotFoundError("Algorithm configuration file not found")
-        
+    
     with open(config_path) as f:
         config = yaml.safe_load(f)
     
@@ -43,12 +42,6 @@ def load_algorithms() -> Dict:
     if 'mediapipe' in config['algorithms']:
         algorithms['MediaPipe'] = MediaPipeFaceDetector(
             config['algorithms']['mediapipe']
-        )
-    
-    # Add RetinaFace
-    if 'retinaface' in config['algorithms']:
-        algorithms['RetinaFace'] = RetinaFaceDetector(
-            config['algorithms']['retinaface']
         )
     
     # Add YOLOv8-Face
